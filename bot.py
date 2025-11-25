731dab17c7f4841a1c11784b456075d4ca17a8c4
Juan David <juandavidgodoy@hotmail.com>
2025-11-25 18:35:17 -0500

    Add help_command=None to bot initialization for improved command help handling

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix=Config.COMMAND_PREFIX, intents=intents)
+bot = commands.Bot(command_prefix=Config.COMMAND_PREFIX, intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
